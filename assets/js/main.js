ee24535abaa6c476d9abeb68820d020b2530f455
Rachel K <rkahn08@gmail.com>
2017-04-27 18:33:35 -0400

    (some) scss refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ console.log("Hello World from main.js!");
 
 $(document).ready(function() {
 	$('.navigation').click(function() {
-		$('.right-side').toggleClass('invisible');
-		$('.left-side').toggleClass('invisible');
+		$('.gopher-side').toggleClass('invisible');
+		$('.carl-side').toggleClass('invisible');
 	})
 })

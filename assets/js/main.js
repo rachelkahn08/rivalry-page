3dfe79926b7be2cd5fde6ac2e1cf6a7afc0e4ee1
Rachel K <rkahn08@gmail.com>
2017-04-27 20:53:15 -0400

    updated tablet stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,18 @@ console.log("Hello World from main.js!");
 
 $(document).ready(function() {
 	$('.navigation').click(function() {
-		$('.gopher-side').toggleClass('invisible');
-		$('.carl-side').toggleClass('invisible');
-	})
-})
+
+		function toggleVisibility() {
+			$('.gopher-side').toggleClass('invisible');
+			$('.carl-side').toggleClass('invisible');
+		};
+
+		toggleVisibility();
+
+		if ( $('.gopher-side').classList.contains('.invisible') ) {
+			document.body.setAttribute('backgroundColor', '$carlGrey');
+		} else if ( $('.carl-side').classList.contains('.invisible') ) {
+			document.body.setAttribute('backgroundColor', '$gopherGrey');
+		};
+	});
+});
